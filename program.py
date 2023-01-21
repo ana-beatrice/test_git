c14b35215d87febc085857ecdf7eab159160f20b
ana-beatrice <Beatriciana@yahoo.com>
2023-01-21 20:57:51 +0200

    added diff fct

--- program.py
+++ program.py
@@ -1,8 +1,13 @@
 from contextlib import nullcontext
 
 
-def div(a,b):
+def div(a, b):
     if b == 0:
         print("you can't divide a number by 0")
         return None
-    return a/b
+    return a / b
+
+
+def diff(a, b):
+    return a - b
+    #as
