98f224223b5a06b699f957c690ad726d312f6cb9
florinIuga <floriniuga19988@gmail.com>
2023-01-21 20:50:47 +0200

    added sum fct

--- program.py
+++ program.py
@@ -3,6 +3,10 @@ from contextlib import nullcontext
 
 def div(a,b):
     if b == 0:
+        print("Ana are mere")
         print("you can't divide a number by 0")
         return None
     return a/b
+
+def sum(a, b):
+    return a + b
